45ec539e594c41e7462ee7b3e14e9709a925da05
ehsan <savvyversa@gmail.com>
2022-07-16 14:15:04 +0430

    fix(test): update megaMenu

--- src/components/market/layout/NtmMegaMenu.vue
+++ src/components/market/layout/NtmMegaMenu.vue
@@ -1,19 +1,74 @@
 <template>
   <div>
-    <a
-        v-for="category in dummyData"
-        :key="category.key"
-        class="hover flex items-center py-3 text-right text-sm"
-    >
-      <svg class="h-4 w-4" fill="currentColor">
-        <use :href="category.icon"></use>
-      </svg>
-      <span class="pr-2"> {{ category.title }}</span>
-    </a>
+    <div class="megaMenu mb-16 bg-white px-0 shadow-xl sm:mb-0">
+      <div class="flex h-fit w-full justify-between">
+        <div class="w-1/6 border-l border-gray-200">
+          <div>
+            <a
+                v-for="(category,index) in items"
+                :key="category.key"
+                :href="category.key"
+                @mouseover="megaMenuMainCategoryIndex = index"
+                class="hover flex items-center py-3 text-right text-sm"
+            >
+              <svg class="h-4 w-4" fill="currentColor">
+                <use :href="category?.icon"></use>
+              </svg>
+              <span class="pr-2"> {{ category?.title }}</span>
+            </a>
+          </div>
+        </div>
+        <div v-if="mainCategory" class="w-full px-4">
+          <div class="flex">
+            <div class="w-4/5">
+              <a class="flex cursor-pointer py-4 text-right text-[11px] text-gray-700">
+                <span class="pl-1">همه </span>
+                <span>{{ mainCategory?.title }}</span>
+                <svg class="h-4 w-4" fill="currentColor">
+                  <use href="#chevronLeft"></use>
+                </svg>
+              </a>
+
+              <div class="flex h-[420px] flex-col flex-wrap pb-5">
+                <template v-for="category in mainCategory.categories">
+                  <NuxtLink :to="category.key">
+                    <a class="mt-1 flex  py-1 hover:text-red-600">
+                      <span class="w-0.5 bg-red-600"></span>
+                      <span class="pl-1 pr-2 font-bold"> {{ category?.title }}</span>
+
+                      <svg class="h-3 w-3" fill="currentColor">
+                        <use href="#chevronLeft"></use>
+                      </svg>
+                    </a>
+                  </NuxtLink>
+                  <template v-for="subCategory in category.subCategories">
+                    <NuxtLink :to="subCategory.key">
+                      <a class="mt-1 flex py-1 text-[11px] text-gray-500  hover:text-red-600">
+                        <span class="px-2"> {{ subCategory?.title }}</span>
+                      </a>
+                    </NuxtLink>
+                  </template>
+                </template>
+              </div>
+            </div>
+            <div class="w-1/5 pt-4">
+              <a class="">
+                <img
+                    src="/assets/img/MegaMenuRandom.jpg"
+                    class="rounded-lg"
+                    alt=""
+                />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script lang="ts" setup>
-const dummyData = [
+const megaMenuMainCategoryIndex = ref(0)
+const dummyData = ([
   {
     key: 'electronic',
     title: 'کالای دیجیتال',
@@ -847,5 +902,9 @@ const dummyData = [
     icon: '#nativeBusiness',
     categories: [],
   },
-];
+]);
+const items = ref(dummyData);
+const mainCategory = computed(() => {
+  return items.value[megaMenuMainCategoryIndex.value];
+});
 </script>
