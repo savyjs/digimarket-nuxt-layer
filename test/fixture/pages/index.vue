2e4b2e6c3ed49fecef5a16b783df710814af5fc5
ehsan <savvyversa@gmail.com>
2023-01-17 16:02:26 +0330

    chore(index): fix link

--- test/fixture/pages/index.vue
+++ test/fixture/pages/index.vue
@@ -8,7 +8,7 @@
         <nuxt-link class="btn bg-green-500 btn-primary" to="/market/profile/orders">Orders</nuxt-link>
         <nuxt-link class="btn bg-green-500 btn-primary" to="/market/profile/order-details/1">view Sample Order
         </nuxt-link>
-        <nuxt-link class="btn bg-green-500 btn-gray-outlined" to="/market/products/1">Product page</nuxt-link>
+        <nuxt-link class="btn bg-green-500 btn-gray-outlined" to="/market/products/8364953">Product page</nuxt-link>
       </div>
     </div>
     <div class="m-5">
@@ -20,7 +20,7 @@
         <nuxt-link class="btn bg-black btn-primary" href="https://diginext.savyjs.com/checkout/cart/2">Cart 2</nuxt-link>
         <nuxt-link class="btn bg-black btn-primary" href="https://diginext.savyjs.com/checkout/cart/3">Cart 3</nuxt-link>
         <nuxt-link class="btn bg-black btn-primary" href="https://diginext.savyjs.com/checkout/cart/4">Cart 4</nuxt-link>
-        <nuxt-link class="btn bg-black btn-primary" href="https://diginext.savyjs.com/checkout/shpping">Shipping</nuxt-link>
+        <nuxt-link class="btn bg-black btn-primary" href="https://diginext.savyjs.com/checkout/shipping">Shipping</nuxt-link>
         <nuxt-link class="btn bg-black btn-primary" href="https://diginext.savyjs.com/checkout/payment">Payment</nuxt-link>
       </div>
     </div>
@@ -28,13 +28,13 @@
       <p class="">Angular pages:</p>
       <div class="gap-3 flex">
         <nuxt-link class="btn btn-primary" href="https://digiangular.savyjs.com/">Market Landing</nuxt-link>
-        <nuxt-link class="btn btn-primary" href="https://digiangular.savyjs.com/products/1">Product page</nuxt-link>
+        <nuxt-link class="btn btn-primary" href="https://digiangular.savyjs.com/products/8364953">Product page</nuxt-link>
       </div>
     </div>
     <div class="m-5">
       <p class="">Pure HTML pages:</p>
       <div class="gap-3 flex">
-        <nuxt-link class="btn btn-gray-outlined" to="/market/products/1">landing 2</nuxt-link>
+        <nuxt-link class="btn btn-gray-outlined" to="/market/products/8364953">landing 2</nuxt-link>
       </div>
     </div>
   </div>
