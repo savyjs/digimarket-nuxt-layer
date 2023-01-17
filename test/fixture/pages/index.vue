37ddbd0f78a93f7c911897246e6e5f7bb2401f1a
ehsan <savvyversa@gmail.com>
2023-01-17 14:01:50 +0330

    chore(ci): ignore bash

--- test/fixture/pages/index.vue
+++ test/fixture/pages/index.vue
@@ -1,3 +1,10 @@
 <template>
-  <div>Hello world!</div>
+  <div class="flex flex-col gap-3">
+    Welcome!
+    <br />
+    <nuxt-link to="/market">Market Landing</nuxt-link>
+    <nuxt-link to="/market/profile">Profile</nuxt-link>
+    <nuxt-link to="/market/profile/orders">Orders</nuxt-link>
+    <nuxt-link to="/market/profile/orders/1">view Order</nuxt-link>
+  </div>
 </template>
