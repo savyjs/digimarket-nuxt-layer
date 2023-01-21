f9db07ae1cca20f9c2822b42d2a3dda031c1bd6b
ehsan <savvyversa@gmail.com>
2023-01-21 17:49:36 +0330

    chore: fix tw-tmk plugin

--- test/fixture/postcss.config.js
+++ test/fixture/postcss.config.js
@@ -1,11 +1,13 @@
+plugins = process.env.MODULE_DEV == 'digimarket_dev' ? require('../../tailwind/src/postcss.config') : [
+    'postcss-partial-import',
+    'postcss-import',
+    'tailwindcss',
+    'autoprefixer',
+    'postcss-nested',
+    'tailwindcss/nesting',
+    'postcss-import-ext-glob',
+];
+
 module.exports = {
-    plugins: [
-        'postcss-partial-import',
-        'postcss-import-ext-glob',
-        'postcss-import',
-        'tailwindcss',
-        'autoprefixer',
-        'postcss-nested',
-        'tailwindcss/nesting'
-    ],
+    plugins: plugins
 }
