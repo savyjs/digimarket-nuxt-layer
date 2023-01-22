1ac2a90a4fc55d15515d3873d7d23348293ae953
ehsan <savvyversa@gmail.com>
2023-01-22 13:03:06 +0330

    chore(tmk): fix builder

--- test/fixture/postcss.config.js
+++ test/fixture/postcss.config.js
@@ -1,11 +1,11 @@
 plugins = process.env.MODULE_DEV == 'digimarket_dev' ? require('../../tailwind/src/postcss.config') : [
+    'postcss-import-ext-glob',
     'postcss-partial-import',
     'postcss-import',
     'tailwindcss',
     'autoprefixer',
     'postcss-nested',
     'tailwindcss/nesting',
-    'postcss-import-ext-glob',
 ];
 
 module.exports = {
