ca10d4562d4a42791164023477477921bbfbe587
ehsan <savvyversa@gmail.com>
2023-06-06 14:19:39 +0330

    chore: test CD

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,7 +14,7 @@ export default defineNuxtConfig({
         join(currentDir, "./assets/styles/ntm.scss"),
         join(currentDir, "./tailwind/all.css")
     ],
-    nitro:{
-        preset: "netlify-edge"
+    nitro: {
+        preset: "node"
     }
 })
