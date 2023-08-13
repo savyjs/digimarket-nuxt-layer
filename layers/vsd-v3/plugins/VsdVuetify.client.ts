3c9476629e45c20a8263b9e8e909b7c2c5c46688
savvy <savvyversa@gmail.com>
2023-08-13 19:15:03 +0330

    feat: add VSD-3 layer

---
+++ layers/vsd-v3/plugins/VsdVuetify.client.ts
@@ -0,0 +1,21 @@
+import {defineNuxtPlugin} from "nuxt/app";
+
+
+// Vuetify
+import 'vuetify/styles'
+import {createVuetify} from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+export default defineNuxtPlugin(nuxtApp => {
+
+
+    const vuetify = createVuetify({
+        ssr: true,
+        components,
+        directives,
+    });
+
+    vuetify.install(nuxtApp.vueApp);
+
+})
