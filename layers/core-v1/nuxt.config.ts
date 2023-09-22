534224aca6e4661d304ae37fe9da788a63facead
savvy <savvyversa@gmail.com>
2023-09-23 01:31:46 +0330

    chore: added dotenv strapi module

--- layers/core-v1/nuxt.config.ts
+++ layers/core-v1/nuxt.config.ts
@@ -1,14 +1,20 @@
 import {defineNuxtConfig} from "nuxt/config";
 import {fileURLToPath} from 'url'
 import {dirname, join} from 'path'
+import dotenv from "dotenv"
+dotenv.config()
 
 const currentDir = dirname(fileURLToPath(import.meta.url))
 
 export default defineNuxtConfig({
     modules: [
         '@pinia/nuxt',
-        '@nuxtjs/tailwindcss'
+        '@nuxtjs/tailwindcss',
+        '@nuxtjs/strapi'
     ],
+    strapi: {
+        url: process.env?.API_URL || "/api"
+    },
     css: [
         //join(currentDir, "./assets/styles/ntm.scss"),
         "@tabler/icons-webfont/tabler-icons.min.css",
