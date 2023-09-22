98bad1a02cb6239e146bff154dc3a59b98a334d3
savvy <savvyversa@gmail.com>
2023-09-22 14:07:58 +0330

    feat: add all layers

---
+++ layers/commerce-v1/pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <header>
+      <!-- Header content here -->
+    </header>
+    <section class="hero">
+      <!-- Hero section content here -->
+    </section>
+    <!-- Rest of the landing page content here -->
+  </div>
+</template>
+<script>
+export default {
+  // Your component logic here
+}
+</script>
+<style scoped>
+/* Your component styles here */
+</style>
+
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,19 +9,19 @@ export default defineNuxtConfig({
     // This is the list of available modules
     extends: [
         "layers/core-v1",
-        // "layers/auth-v1",
+        "layers/auth-v1",
         "layers/commerce-v1",
-        // "layers/inventory-v1",
-        // "layers/blog-v1",
-        // "layers/vuetify-strapi-dashboard-v3",
+        "layers/inventory-v1",
+        "layers/blog-v1",
+        "layers/vuetify-strapi-dashboard-v3",
     ],
     modules: [
         // "@nuxtjs/tailwindcss",
         '@nuxtjs/strapi'
     ],
     css: [
-        // join(currentDir, "./assets/styles/ntm.scss"),
-        // join(currentDir, "./tailwind/all.css")
+        join(currentDir, "./assets/styles/ntm.scss"),
+        join(currentDir, "./tailwind/all.css")
     ],
     // nitro: {
     //     preset: "node"
