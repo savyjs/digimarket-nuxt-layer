89be0a4ede4f9bf498659d1c1637eabe45582566
savvy <savvyversa@gmail.com>
2023-09-23 01:31:01 +0330

    chore: added strapi again

--- layers/vuetify-strapi-dashboard-v3/nuxt.config.ts
+++ layers/vuetify-strapi-dashboard-v3/nuxt.config.ts
@@ -1,4 +1,14 @@
+import dotenv from "dotenv"
+
+dotenv.config()
+
 export default defineNuxtConfig({
+    modules: [
+        '@nuxtjs/strapi'
+    ],
+    strapi: {
+        url: process.env?.API_URL || "/api"
+    },
     vite: {
         ssr: {
             noExternal: ['vuetify']
