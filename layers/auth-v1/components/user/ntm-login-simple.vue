e1df198379c49f60d3bb9a5d2d5568007b35642c
savvy <savvyversa@gmail.com>
2023-09-23 14:52:31 +0330

    feat: add ntm-simple-auth-pages

---
+++ layers/auth-v1/components/user/ntm-login-simple.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="flex justify-center items-center h-screen">
+    <div class="w-1/3">
+      <h1 class="text-2xl font-bold mb-4">Login Form</h1>
+      <form>
+        <div class="mb-4">
+          <label for="username" class="block text-gray-700 text-sm font-bold mb-2">Username:</label>
+          <input type="text" id="username" class="border border-gray-400 rounded px-3 py-2 w-full">
+        </div>
+        <div class="mb-4">
+          <label for="password" class="block text-gray-700 text-sm font-bold mb-2">Password:</label>
+          <input type="password" id="password" class="border border-gray-400 rounded px-3 py-2 w-full">
+        </div>
+        <button type="submit" class="bg-blue-700 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Login</button>
+      </form>
+    </div>
+  </div>
+</template>
---
+++ layers/auth-v1/components/user/ntm-register-simple.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="flex justify-center items-center h-screen">
+    <div class="w-1/3">
+      <h1 class="text-3xl font-bold mb-6">Register</h1>
+      <form>
+        <div class="mb-4">
+          <label for="name" class="block mb-2">Name</label>
+          <input type="text" id="name" class="w-full border border-gray-300 rounded px-3 py-2">
+        </div>
+        <div class="mb-4">
+          <label for="email" class="block mb-2">Email</label>
+          <input type="email" id="email" class="w-full border border-gray-300 rounded px-3 py-2">
+        </div>
+        <div class="mb-4">
+          <label for="password" class="block mb-2">Password</label>
+          <input type="password" id="password" class="w-full border border-gray-300 rounded px-3 py-2">
+        </div>
+        <div class="mb-6">
+          <label for="confirm-password" class="block mb-2">Confirm Password</label>
+          <input type="password" id="confirm-password" class="w-full border border-gray-300 rounded px-3 py-2">
+        </div>
+        <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded">Register</button>
+      </form>
+    </div>
+  </div>
+</template>
