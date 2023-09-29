4b8b3a58a4da2a37a49e7566c38f89e299f52ed9
savvy <savvyversa@gmail.com>
2023-09-29 16:17:57 +0330

    fix: load dotenv

--- layers/auth-v1/nuxt.config.ts
+++ layers/auth-v1/nuxt.config.ts
@@ -1,7 +1,7 @@
 import {defineNuxtConfig} from "nuxt/config";
-
+import dotenv from "dotenv"
+dotenv.config()
 const schemaName = process?.env?.AUTH_SCHEMA ?? 'ntm' //other options is vsd
-
 export default defineNuxtConfig({
     extends: [
         `layers/${schemaName}`
