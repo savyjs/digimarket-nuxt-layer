cd4bcd9a3199f825a2dece2bf7fb626a99600708
savvy <savvyversa@gmail.com>
2023-09-29 23:01:44 +0330

    fix: add modules and veeValidate configs

--- layers/vuetify-strapi-dashboard-v3/nuxt.config.ts
+++ layers/vuetify-strapi-dashboard-v3/nuxt.config.ts
@@ -1,17 +1,33 @@
 import dotenv from "dotenv"
+
 dotenv.config()
 dotenv.populate(process.env, {AUTH_SCHEMA: 'vsd'})
 
 export default defineNuxtConfig({
     modules: [
-        '@nuxtjs/strapi'
+        '@nuxtjs/strapi',
+        '@vee-validate/nuxt',
+        '@pinia/nuxt'
     ],
     extends: [
         "digimarket-nuxt-layer-auth"
     ],
+    veeValidate: {
+        // disable or enable auto imports
+        autoImports: true,
+        componentNames: {
+            Form: 'VeeForm',
+            Field: 'VeeField',
+            FieldArray: 'VeeFieldArray',
+            ErrorMessage: 'VeeErrorMessage',
+        },
+    },
     strapi: {
         url: process.env?.API_URL || "/api"
     },
+    css: [
+        "@tabler/icons-webfont/tabler-icons.min.css",
+    ],
     vite: {
         ssr: {
             noExternal: ['vuetify']
