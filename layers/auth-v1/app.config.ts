b94ed49025098dd89a43cb2537082ff314b0fae4
savvy <savvyversa@gmail.com>
2023-09-29 15:26:28 +0330

    feat: add authRoutes and authNavigate to each layer

--- layers/auth-v1/app.config.ts
+++ layers/auth-v1/app.config.ts
@@ -1,14 +1,5 @@
 const auth_schema = process?.env?.auth_schema ?? 'ntm' //other options is vsd
 
 export default defineAppConfig({
-    authSchema: auth_schema,
-    authRouting: {
-        login: '/auth/login',
-        register: '/auth/register',
-        verify: '/auth/verify',
-        forgot: '/auth/forgot',
-        home: '/',
-        profile: '/profile',
-        admin: '/admin'
-    }
+    authSchema: auth_schema
 })
